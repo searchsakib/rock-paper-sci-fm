f0bfdf519ca443700e279acae36c9869a7e57fcd
searchsakib <searchsakib@gmail.com>
2024-11-23 21:00:46 +0600

    click icon demo

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,4 +1,4 @@
-import { defaultView, rulesModal } from "./selectors.js";
+import { defaultView, rulesModal, vsView } from "./selectors.js";
 
 // toggling the modal
 export const toggleRulesModal = () => {
@@ -10,4 +10,10 @@ export const toggleRulesModal = () => {
 export const closeModalBtn = () => {
   defaultView.classList.toggle("hidden");
   rulesModal.classList.toggle("hidden");
+};
+
+// selecting from rock,papers,scissors
+export const showVsMode = () => {
+  defaultView.classList.toggle("hidden");
+  vsView.classList.toggle("hidden");
 };
--- src/js/selectors.js
+++ src/js/selectors.js
@@ -1,4 +1,6 @@
 export const defaultView = document.getElementById("default_view");
+export const vsView = document.getElementById("vs_view");
 export const rulesModal = document.getElementById("rules_modal");
 export const rulesModalBtn = document.getElementById("rules_modal_btn");
 export const modalCloseBtn = document.getElementById("close_btn");
+export const paperBtn = document.getElementById("paper_btn");
