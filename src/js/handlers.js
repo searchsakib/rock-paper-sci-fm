fbb4b5959e25bc0de0c458418d6c6d4da2cb7ee4
searchsakib <searchsakib@gmail.com>
2024-11-23 19:28:41 +0600

    code reduced by using toggle feature

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -2,21 +2,12 @@ import { defaultView, rulesModal } from "./selectors.js";
 
 // toggling the modal
 export const toggleRulesModal = () => {
-  if (
-    !defaultView.classList.contains("hidden") &&
-    rulesModal.classList.contains("hidden")
-  ) {
-    defaultView.classList.add("hidden");
-    rulesModal.classList.remove("hidden");
-    console.log("button clicked");
-  } else {
-    defaultView.classList.remove("hidden");
-    rulesModal.classList.add("hidden");
-  }
+  defaultView.classList.toggle("hidden");
+  rulesModal.classList.toggle("hidden");
 };
 
 // close modal btn
 export const closeModalBtn = () => {
-  defaultView.classList.remove("hidden");
-  rulesModal.classList.add("hidden");
+  defaultView.classList.toggle("hidden");
+  rulesModal.classList.toggle("hidden");
 };
